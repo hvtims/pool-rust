df229eb02528998bf03d51ea3631de448f9288b0
thatim <timo.goog03@gmail.com>
2025-09-04 12:16:16 +0100

    looping

--- looping/target/debug/deps/looping-24c60e78ba5bb720.d
+++
@@ -1,5 +0,0 @@
-/home/thatim/Desktop/piscine-rust/looping/target/debug/deps/looping-24c60e78ba5bb720: src/main.rs
-
-/home/thatim/Desktop/piscine-rust/looping/target/debug/deps/looping-24c60e78ba5bb720.d: src/main.rs
-
-src/main.rs:
--- looping/target/debug/looping.d
+++
@@ -1,1 +0,0 @@
-/home/thatim/Desktop/piscine-rust/looping/target/debug/looping: /home/thatim/Desktop/piscine-rust/looping/src/main.rs
