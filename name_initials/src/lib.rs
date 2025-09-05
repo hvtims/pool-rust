d0f3aae53361a2d807666606183bfdc7d27ecb02
thatim <timo.goog03@gmail.com>
2025-09-05 11:58:43 +0100

    name_initials

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,2 +1,15 @@
 pub fn initials(names: Vec<&str>) -> Vec<String> {
+    let mut ghadir = vec![];
+    for i in names {
+        let mut temp = String::new();
+        for k in i.split(" ").collect::<Vec<_>>() {
+            temp.push(k.chars().next().unwrap());
+            temp.push('.');
+
+            temp.push(' ');
+        }
+        temp.pop();
+        ghadir.push(temp);
+    }
+    return ghadir
 }
