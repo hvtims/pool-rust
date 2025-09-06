d5360d4f7883335ba3503631b3eac3f3dec4f5fb
thatim <timo.goog03@gmail.com>
2025-09-06 16:51:23 +0100

    collect

--- collect/src/lib.rs
+++ collect/src/lib.rs
@@ -1,7 +1,6 @@
 pub fn bubble_sort(arr: &mut [i32]) {
-    let n = arr.len();
-    for (i, _) in arr.iter().enumerate() {
-        for (j, _) in arr.iter().enumerate().take(n - i - 1) {
+    for i in 0..arr.len() {
+        for j in 0..(arr.len() - i - 1) {
             if arr[j] > arr[j + 1] {
                 arr.swap(j, j + 1);
             }
