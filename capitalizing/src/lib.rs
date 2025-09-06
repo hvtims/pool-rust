510f3192821dc3e9b779c9c30e35ec2335093145
thatim <timo.goog03@gmail.com>
2025-09-06 12:45:46 +0100

    capitalazing

---
+++ capitalizing/src/lib.rs
@@ -0,0 +1,41 @@
+pub fn capitalize_first(input: &str) -> String {
+    let arr  : Vec<_> = input.chars().collect();
+    let mut newstrg = String::new();
+    for (i , v ) in arr.into_iter().enumerate(){
+        if i == 0 {
+            newstrg += &v.to_uppercase().to_string();
+            continue;
+        }
+        newstrg+= &v.to_string();
+    }
+    return newstrg;
+}
+
+pub fn title_case(input: &str) -> String {
+    let splited : Vec<_>= input.split(" ").collect();
+    let mut newstr = String::new();
+    for ( i , v) in splited.clone().into_iter().enumerate(){
+       if i != splited.len(){
+           newstr+= &capitalize_first(v);
+           newstr += " "
+       }
+    }
+    return newstr.to_string()
+}
+
+pub fn change_case(input: &str) -> String {
+    let waw : Vec<_> = input.chars().collect();
+    let mut newstr = String::new();
+    for i in waw{
+        if i.is_lowercase(){
+            newstr += &i.to_uppercase().to_string();
+            continue;
+        }else if i.is_uppercase(){
+            newstr += &i.to_lowercase().to_string();
+            continue;
+        }else{
+            newstr+= &i.to_string();
+        }
+    }
+    return newstr
+}
