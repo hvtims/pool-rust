4d6ce547f200ca68633f7ed1b8a3e1c65e60a1a0
thatim <timo.goog03@gmail.com>
2025-09-11 16:31:29 +0100

    error_types

---
+++ error_types/src/lib.rs
@@ -0,0 +1,64 @@
+use chrono::Utc;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct FormError {
+    pub form_values: (String, String),
+    pub date: String,
+    pub err: String,
+}
+
+impl FormError {
+    pub fn new(field_name: &str, field_value: String, err: &str) -> Self {
+        let date = Utc::now().format("%Y-%m-%d %H:%M:%S").to_string();
+
+        FormError {
+            form_values: (field_name.to_string(), field_value),
+            date,
+            err: err.to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Form {
+    pub name: String,
+    pub password: String,
+}
+
+impl Form {
+    pub fn new(name: String, password: String) -> Self {
+        Self { name, password }
+    }
+
+    pub fn validate(&self) -> Result<(), FormError> {
+        if self.name.is_empty() {
+            return Err(FormError::new(
+                "name",
+                self.name.clone(),
+                "Username is empty",
+            ));
+        }
+
+        if self.password.len() < 8 {
+            return Err(FormError::new(
+                "password",
+                self.password.clone(),
+                "Password should be at least 8 characters long",
+            ));
+        }
+
+        let has_alpha = self.password.chars().any(|c| c.is_ascii_alphabetic());
+        let has_numeric = self.password.chars().any(|c| c.is_ascii_digit());
+        let has_symbol = self.password.chars().any(|c| !c.is_ascii_alphanumeric());
+
+        if !(has_alpha && has_numeric && has_symbol) {
+            return Err(FormError::new(
+                "password",
+                self.password.clone(),
+                "Password should be a combination of ASCII letters, numbers, and symbols",
+            ));
+        }
+
+        Ok(())
+    }
+}
