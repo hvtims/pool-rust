d486dec686b01c3fc662fa9aeda278cba3dd5ef4
thatim <timo.goog03@gmail.com>
2025-09-11 12:45:18 +0100

    handling

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -1,16 +1,19 @@
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 use std::path::Path;
 use std::io::Write;
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
     match File::open(path) {
         Ok(_) => {
-            let mut ghadir  = File::open(path).unwrap();
+            let mut ghadir = OpenOptions::new()
+                .append(true)
+                .open(path)
+                .unwrap();
             let _ = ghadir.write_all(content.as_bytes());
         }
         Err(_) => {
-            let mut _ghadir = File::create(path).unwrap();
-           let _= _ghadir.write_all(content.as_bytes());
+            let mut ghadir = File::create(path).unwrap();
+            let _ = ghadir.write_all(content.as_bytes());
         }
     }
 }
