41c873ec6a6cc96aabc51a0b6fe28864ea944814
thatim <timo.goog03@gmail.com>
2025-09-22 15:10:12 +0100

    commits

--- commits_stats/src/lib.rs
+++ commits_stats/src/lib.rs
@@ -1,34 +1,30 @@
 use std::collections::HashMap;
-use chrono::DateTime;
 use json::JsonValue;
-
-pub fn commits_per_week(data: &JsonValue) -> HashMap<String, u32> {
-    let mut week_counts = HashMap::new();
-
-    if let JsonValue::Array(commits) = data {
-        for commit in commits {
-            if let Some(date_str) = commit["commit"]["author"]["date"].as_str() {
-                if let Ok(datetime) = DateTime::parse_from_rfc3339(date_str) {
-                    let week = datetime.format("%Y-W%V").to_string();
-                    *week_counts.entry(week).or_insert(0) += 1;
-                }
-            }
-        }
-    }
-
-    week_counts
-}
+use chrono::{DateTime, Utc, Datelike};
 
 pub fn commits_per_author(data: &JsonValue) -> HashMap<String, u32> {
-    let mut author_counts = HashMap::new();
+    let mut map = HashMap::new();
+    for commit in data.members() {
+        if let Some(login) = commit["author"]["login"].as_str() {
+            *map.entry(login.to_string()).or_insert(0) += 1;
+        }
+    }
 
-    if let JsonValue::Array(commits) = data {
-        for commit in commits {
-            if let Some(author_name) = commit["author"]["login"].as_str() {
-                *author_counts.entry(author_name.to_string()).or_insert(0) += 1;
+    map
+}
+
+pub fn commits_per_week(data: &JsonValue) -> HashMap<String, u32> {
+    let mut map = HashMap::new();
+    for commit in data.members() {
+        if let Some(date_str) = commit["commit"]["author"]["date"].as_str() {
+            if let Ok(date) = DateTime::parse_from_rfc3339(date_str) {
+                let date_utc: DateTime<Utc> = date.with_timezone(&Utc);
+                let iso_week = date_utc.iso_week();
+                let key = format!("{}-W{}", iso_week.year(), iso_week.week());
+                *map.entry(key).or_insert(0) += 1;
             }
         }
     }
 
-    author_counts
+    map
 }
